839f001dd4c80f1caf72b17fd00e398d9dd16acc
hthuelig <51367725+hthuelig@users.noreply.github.com>
2019-06-04 14:28:35 +0200

    hi

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,6 +2,6 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract HelloWorld {
     function sayhello() public pure returns(string memory) {
-        return "Hello World Motherfuckerghhhhh!";
+        return "Hello World Motherfucker!";
     }
 }
