8a8939b5d0d5433770cbae91d0c8f150d92b0cbc
Marvin Klose <marvin@Marvins-MacBook-Pro.local>
2019-06-04 14:17:55 +0200

    test

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,10 +2,6 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract HelloWorld {
     function sayhello() public pure returns(string memory) {
-<<<<<<< HEAD
-        return "Hello World! Nur der BVB!";
-=======
         return "Hello World Motherfucker!";
->>>>>>> 3d38714fcf0b541508da15be8fe86b81f96124a6
     }
 }
