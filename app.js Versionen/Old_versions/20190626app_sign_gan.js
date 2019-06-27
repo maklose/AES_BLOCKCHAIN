88957fd121f0566e802afec6cae0b6b18848807d
Moerchel <pascal.moerchel@miele.com>
2019-06-27 21:31:04 +0200

    commit calculate nonce & gasPrice

--- app.js Versionen/Old_versions/20190626app_sign_gan.js
+++ app.js Versionen/Old_versions/20190626app_sign_gan.js
@@ -19,43 +19,39 @@ var iPrivateKey;
 
 
 //testing: Test Data initialization within coding
-iGasPrice = 60000;
-iGasLimit = 50000;
-iPrivateKey = '15EF5EB48D8D1A514C7C392483209ADBF737E45ADB962633240437BFB9352231';
-iFromAdress = '0xF6b7Aef42305A4A24fbD28db9B7376C2267FcD3A';
-iToAdress = '0x3eC7ef9696d5bbedFf3688Cc76bc3e05A06c7E55';
+iGasPrice = 20000000000;
+iGasLimit = 6721975;
+iPrivateKey = '8e9b1bc69ddf2feb5fb710bd863b84fbd218e17bbc39459b7ebcc064041e6fbd';
+iToAdress = '0x5ac12B90f9a6653eE7EdE68c78133B79B67a057C';
 iValue = 1;
 iData = 'hier sind ein paar input data';
 
 
-//prepare values to Hex Code
+//testing: Show Output in Console
+const hexGasPrice = web3.utils.toHex(iGasPrice.toString());
 const hexGasLimit = web3.utils.toHex(iGasLimit.toString());
 const value = web3.utils.toHex(web3.utils.toWei(iValue.toString(), "ether"));
 const data = web3.utils.toHex(iData);
+console.log("Output Transaction values:");
+console.log("");
+console.log("HexGasPrice: " + hexGasPrice);
+console.log("hexGasLimit: " + hexGasLimit);
+console.log("Value: " + value);
+console.log("Data: " + data);
+console.log("");
 
 
-//all functions in single callback method:
+//To-Do: nonce kalkulieren
+//var nonce = web3.utils.toHex(11);
+var iNonceValue = web3.utils.toHex(4);
+console.log("Nonce value: " + iNonceValue);
+//To-Do: GasPrice abfragen
 
-//1st Calculate Nonce:
-web3.eth.getTransactionCount(iFromAdress, (err, txCount) =>
-{
-  //Estimate Gas Price
-  web3.eth.estimateGas({ to: iToAdress, data: data}, (err, gasEstimate) => 
-  {
-  //testing: Show Output in Console
-  console.log("Output Transaction values:");
-  console.log("");
-  console.log("GasEstimate: " + gasEstimate);
-  console.log("hexGasLimit: " + hexGasLimit);
-  console.log("Value: " + value);
-  console.log("Data: " + data);
-  console.log("Nonce value: " + txCount);
-  console.log("");
 
-  //Create Transaction Object with raw data
-  const rawTx = {
-  nonce:      web3.utils.toHex(txCount),
-  gasPrice:   web3.utils.toHex(gasEstimate.toString()),
+//Create Transaction Object with raw data
+var rawTx = {
+  nonce:      "0x" + iNonceValue,
+  gasPrice:   web3.utils.toHex(iGasPrice.toString()),
   gasLimit:   web3.utils.toHex(iGasLimit.toString()),
   to:         iToAdress,
   value:      value,
@@ -69,6 +65,6 @@ tx.sign(privateKey);
 
 //send signed Transaction to Blockchain
 const serializedTx = tx.serialize();
+console.log('0x' + serializedTx.toString('hex'));
 web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), handleReceipt);
-})
-});
+
--- app.js Versionen/app_sign_gan.js
+++ app.js Versionen/app_sign_gan.js
@@ -20,38 +20,42 @@ var iPrivateKey;
 
 //testing: Test Data initialization within coding
 iGasPrice = 20000000000;
-iGasLimit = 6721975;
+iGasLimit = 50000;
 iPrivateKey = '8e9b1bc69ddf2feb5fb710bd863b84fbd218e17bbc39459b7ebcc064041e6fbd';
+iFromAdress = '0xE479b7a82eb2EB5D5586d7696b8D6b29ABbC4db7'; //for getTransactionCount
 iToAdress = '0x5ac12B90f9a6653eE7EdE68c78133B79B67a057C';
 iValue = 1;
-iData = 'hier sind ein paar input data';
+iData = 'hier sind ein paar input dataasdfasdfasdfasdfaserwearawerfgdafgdsf';
 
 
-//testing: Show Output in Console
-const hexGasPrice = web3.utils.toHex(iGasPrice.toString());
+//prepare values to Hex Code
 const hexGasLimit = web3.utils.toHex(iGasLimit.toString());
 const value = web3.utils.toHex(web3.utils.toWei(iValue.toString(), "ether"));
 const data = web3.utils.toHex(iData);
-console.log("Output Transaction values:");
-console.log("");
-console.log("HexGasPrice: " + hexGasPrice);
-console.log("hexGasLimit: " + hexGasLimit);
-console.log("Value: " + value);
-console.log("Data: " + data);
-console.log("");
 
 
-//To-Do: nonce kalkulieren
-//var nonce = web3.utils.toHex(11);
-var iNonceValue = web3.utils.toHex(4);
-console.log("Nonce value: " + iNonceValue);
-//To-Do: GasPrice abfragen
+//all functions in single callback method:
 
+//1st Calculate Nonce:
+web3.eth.getTransactionCount(iFromAdress, (err, txCount) =>
+{
+  //Estimate Gas Price
+  web3.eth.estimateGas({ to: iToAdress, data: data}, (err, gasEstimate) => 
+  {
+  //testing: Show Output in Console
+  console.log("Output Transaction values:");
+  console.log("");
+  console.log("GasEstimate: " + gasEstimate);
+  console.log("hexGasLimit: " + hexGasLimit);
+  console.log("Value: " + value);
+  console.log("Data: " + data);
+  console.log("Nonce value: " + txCount);
+  console.log("");
 
-//Create Transaction Object with raw data
-var rawTx = {
-  nonce:      "0x" + iNonceValue,
-  gasPrice:   web3.utils.toHex(iGasPrice.toString()),
+  //Create Transaction Object with raw data
+  const rawTx = {
+  nonce:      web3.utils.toHex(txCount),
+  gasPrice:   web3.utils.toHex(gasEstimate.toString()),
   gasLimit:   web3.utils.toHex(iGasLimit.toString()),
   to:         iToAdress,
   value:      value,
@@ -65,6 +69,6 @@ tx.sign(privateKey);
 
 //send signed Transaction to Blockchain
 const serializedTx = tx.serialize();
-console.log('0x' + serializedTx.toString('hex'));
 web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), handleReceipt);
-
+})
+});
