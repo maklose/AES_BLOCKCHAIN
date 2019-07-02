2573b930d89362e853044fdff9f54f9e5d0f39d0
hthuelig <51367725+hthuelig@users.noreply.github.com>
2019-07-02 15:15:49 +0200

    asd

--- contracts/ContractA_HT.sol
+++ contracts/ContractA_HT.sol
@@ -3,12 +3,16 @@ pragma solidity >=0.4.25 <0.6.0;
 
 contract CounterPay1 {
     
+
+
+    
     mapping(address => uint256) MachineDeposits;
     mapping(address => uint256) MachineHours;
-    mapping(address => uint256) ConfirmationMachine;
-    mapping(address => uint256) ConfirmationDienstleister;
+    mapping(constant => uint256) ConfirmationMaschine;
+    mapping(constant => uint256) ConfirmationDienstleister;
     
-    function riseCounterA(uint input)  public payable{
+    
+    function increaseCounter_Balance(uint input)  public payable{
         MachineHours[msg.sender] += input;
         MachineDeposits[msg.sender] += msg.value;
     }
@@ -16,30 +20,29 @@ contract CounterPay1 {
         return MachineDeposits[msg.sender];
      
     }
-    function getCountA() public view returns (uint) {
+    function getCount() public view returns (uint) {
            return MachineHours[msg.sender];
     }
 
-    function CheckLimitCountA() public view returns (bool) {
+    function checkLimitCount() public view returns (bool) {
         if (MachineHours[msg.sender] < 400) return false;
         else return true;
     }
+    
     // Wenn Bestätigungen in Form von Data von beiden Teilnehmern eingegangen sind
     // schmeißt der Smart Contract boolean Werte aus
     
     function ConfirmationMachine1(uint input) public view {
-        address Maschine = msg.sender;
-        ConfirmationMachine + 1;    //eventuell smarter dazu noch den Timestamp zu überprüfen
+       ConfirmationMaschine + 1;    //eventuell smarter dazu noch den Timestamp zu überprüfen
     }
     
     function ConfirmationDienstleister1 (uint input) public view {
-        address Dienstleister = msg.sender;
         ConfirmationDienstleister + 1;
     }
         
     function ConfirmMain () public view returns (bool){
-        if (ConfirmationMachine[CounterPay1] = 1) & (ConfirmationDienstleister[CounterPay1] = 1);
-        return true;
-        else return false
+        if (ConfirmationMaschine = 1 & ConfirmationDienstleister = 1) return true;
+        else return false;
     }
+    
 }
