13179871f47d446434e9ce0d29211301913b9539
agorlick <albert.gorlick@gmail.com>
2019-07-03 14:39:15 +0200

    aaa

--- contracts/Contract_Albert.sol
+++ contracts/Contract_Albert.sol
@@ -8,31 +8,52 @@ contract CountAndDeposit {
     mapping(address => uint256) machineCounter;
     uint counterLimit;
     
+    
+// HIER DEFINIEREN WIR DEN CONTRACT
+     // hier wird der contractOwner definiert
+     // geschieht bei der Erstellung der Contract-Instanz
      constructor() public {
          contractOwner = msg.sender;
      }
-
+     
+     //hier wird der contractPartner definiert
+     //kann nur vom contractPartner definiert werden
+     function setContractPartner(address input) public{
+         require (msg.sender == contractOwner);
+         contractPartner == input;
+     }
+     
+     // legt das Limit für Maschinenstunden fest
+     // kann nur vom contractOwner definiert werden
      function setCounterLimit(uint input) public{
          require (msg.sender == contractOwner);
          counterLimit += input;
      }
      
-     function getCounterLimit() public view returns (uint) {
-         return counterLimit;
-     }
-     
+// HIER SIND FUNKTIONEN, UM DEN CONTRACT ZU NUTZEN
+// ALLE INCREASE FUNKTIONEN     
     // erhöht Counter für Maschinenstunden des owners
     // (also der Maschine, die den Contract angelegt hat)
     function increaseCounter(uint input) public{
         require (msg.sender == contractOwner);
         machineCounter[contractOwner] += input;
     }
+    
     // erhöht Balance des owners 
     // (also die Summe der anteiligen Ether-Zahlungen)
     function increaseBalance(uint input) public payable{
         require (msg.sender == contractOwner);
         machineBalance[contractOwner] += msg.value;
     }
+    
+// HIER KANN MAN DEN STATUS DES CONTRACT PRÜFEN
+// ALLE GET FUNKTIONEN
+     // gibt counterLimit zurück
+     // kann von allen genutzt werden
+     function getCounterLimit() public view returns (uint) {
+         return counterLimit;
+     }
+    
     // überprüft und gibt die Balance des owners 
     // (also die Einzahlungen der Maschine) zurück
     function getBalance() public view returns (uint) {
@@ -44,6 +65,7 @@ contract CountAndDeposit {
     function getCount() public view returns (uint) {
            return machineCounter[contractOwner];
     }
+
     // überprüft, ob das vertraglich festgelegte Limit 
     // der Maschinenstunden (MachineCounter) 
     // erreicht oder überschritten wurde und gibt 
