518347ee91d700792b599adf5a251c6c6e23bfe8
hthuelig <51367725+hthuelig@users.noreply.github.com>
2019-07-03 16:28:20 +0200

    df

--- contracts/ContractA_HT.sol
+++ contracts/ContractA_HT.sol
@@ -1,67 +1,105 @@
 pragma solidity >=0.4.25 <0.6.0;
 
-
-contract CounterPay1 {
+contract CountAndDeposit {
     
-    
-    mapping(address => uint256) MachineDeposits;
-    mapping(address => uint256) MachineHours;
+    address contractOwner;
+    address contractPartner;
+    mapping(address => uint256) machineBalance;
+    mapping(address => uint256) machineCounter;
     mapping(address => bool) ConfirmationMaschine;
     mapping(address => bool) ConfirmationDienstleister;
+    uint counterLimit;
     
     
-    function increaseCounter_Balance(uint input)  public payable{
-        MachineHours[msg.sender] += input;
-        MachineDeposits[msg.sender] += msg.value;
-    }
-    function getBalance() public view returns (uint) {
-        return MachineDeposits[msg.sender];
+// HIER DEFINIEREN WIR DEN CONTRACT
+     // hier wird der contractOwner definiert
+     // geschieht bei der Erstellung der Contract-Instanz
+     constructor() public {
+         contractOwner = msg.sender;
+     }
      
-    }
-    function getCount() public view returns (uint) {
-           return MachineHours[msg.sender];
-    }
-
-    function checkLimitCount() public view returns (bool) {
-        if (MachineHours[msg.sender] < 400) return false;
+     //hier wird der contractPartner definiert
+     //kann nur vom contractPartner definiert werden
+     function setContractPartner(address input) public{
+         require (msg.sender == contractOwner);
+         contractPartner == input;
+     }
+     
+     // legt das Limit für Maschinenstunden fest
+     // kann nur vom contractOwner definiert werden
+     function setCounterLimit(uint input) public{
+         require (msg.sender == contractOwner);
+         counterLimit += input;
+     }
+     
+// HIER SIND FUNKTIONEN, UM DEN CONTRACT ZU NUTZEN
+// ALLE INCREASE FUNKTIONEN     
+    // erhöht Counter für Maschinenstunden des owners
+    // (also der Maschine, die den Contract angelegt hat)
+    function increaseCounter(uint input) public returns (bool) {
+        require (msg.sender == contractOwner);
+        machineCounter[contractOwner] += input;
+        if (machineCounter[contractOwner] < counterLimit) return false;
         else return true;
     }
     
-    // Wenn Bestätigungen in Form von Data von beiden Teilnehmern eingegangen sind
-    // schmeißt der Smart Contract boolean Werte aus
-    // Versuch 1, einfach das gleiche Prinzip wie mit Maschinenstunden, funktioniert aber nur so halb
+    // erhöht Balance des owners 
+    // (also die Summe der anteiligen Ether-Zahlungen)
+    function increaseBalance(uint input) public payable{
+        require (msg.sender == contractOwner);
+        machineBalance[contractOwner] += msg.value;
+    }
+    
+// HIER KANN MAN DEN STATUS DES CONTRACT PRÜFEN
+// ALLE GET FUNKTIONEN
+     // gibt counterLimit zurück
+     // kann von allen genutzt werden
+     function getCounterLimit() public view returns (uint) {
+         return counterLimit;
+     }
+    
+    // überprüft und gibt die Balance des owners 
+    // (also die Einzahlungen der Maschine) zurück
+    function getBalance() public view returns (uint) {
+        return machineBalance[contractOwner];
+    }
+    
+    // überprüft und gibt den Counter des owners
+    // (also die Maschinenstunden der Maschine) zurück
+    function getCount() public view returns (uint) {
+           return machineCounter[contractOwner];
+    }
 
+    // überprüft, ob das vertraglich festgelegte Limit 
+    // der Maschinenstunden (MachineCounter) 
+    // erreicht oder überschritten wurde und gibt 
+    // true für MachineCounter > Limit
+    // false für MachineCounter < Limit
+    function checkCounterLimit() public view returns (bool) {
+        if (machineCounter[contractOwner] < counterLimit) return false;
+        else return true;
+    }
+    
+
+// HIER ÜBERPRÜFEN WIR OB WARTUNGSBESTÄTIGUNG VON DER MASCHINE UND DEM DIENSTLEISTER ERHALTEN WURDE
+    //Alle Funktionen, sowohl Input als auch Output
+    
+    // Hier sendet die Maschine die Bestätigung der Wartung an den Smart Contract
+    // Input "true" für Wartung durchgeführt
+    // Input "false" für Wartung nicht durchgeführt
     function ConfirmationMaschine1(bool input) public {
-        ConfirmationMaschine[msg.sender] = true;
-}
+    require (msg.sender == contractOwner);
+        ConfirmationMaschine[contractOwner] = input;
+    }
 
     function ConfirmationDienstleister1(bool input) public {
-        ConfirmationDienstleister[msg.sender] = true;
-}
+        require (msg.sender == contractPartner);
+        ConfirmationDienstleister[contractPartner] = input;
+    }
 
-    function checkConfirmation(bool) public payable returns (bool) {
-        if (ConfirmationMaschine[msg.sender] = true) return true;
+    function checkConfirmation() public view returns (bool) {
+        if ((ConfirmationMaschine[contractOwner] = true) && (ConfirmationDienstleister[contractPartner] = true)) return true;
         else return false;
-    }
-
-    // Versuch 2, die Maschine und der Dienstleister schreiben beide im data Teil die Bestätigung (bool)
-    // Bedingung ließt beide Tabellen im SC aus und gibt bool-Ergebnis wieder, funktioniert aber noch nicht
-    
-    // Um den obigen Code zu compilen und zu deployen muss der untere Teil
-    // von Zeile 50-64 rausgelöscht werden!
-
-     function ConfirmationMaschine1(bool input) public view {
-       ConfirmationMaschine[msg.sender] = true;    //eventuell smarter dazu noch den Timestamp zu überprüfen
-    }
-    
-    function ConfirmationDienstleister1(bool input) public view {
-        ConfirmationDienstleister[msg.sender] = true;
-    }
-        
-    function ConfirmMain() internal view returns (bool){
-        if ((ConfirmationMaschine[msg.sender], true) and (ConfirmationDienstleister[msg.sender], true)) return true;
-    } else 
-    { return false;
     }
 
 }
