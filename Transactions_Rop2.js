69186bdf7e8cfa24b9e9627f2e201d6e9a301580
dawittmann <daniel_m.wittmann@web.de>
2019-07-17 09:57:22 +0200

    init

--- Transactions_Rop2.js
+++ Transactions_Rop2.js
@@ -1,4 +1,14 @@
-// Anwender: Daniel 
+// User: Daniel 
+//--------------------------------------------------------------------------------//
+// Funktionale Anforderungen Smart Contract Stand 17.07:
+// Transaction: Contract Deploy, Partner, Counter Limit , Balance Limit, increase, Confirmation Owner, Confirmation Partner, Check Confirmation and Send 
+// Call: stamp Owner, stamp Partner, Counter Limit, Balance Limit, Balance , Count , Confirmation Owner, Confirmation Partner, Check Balance Limit 
+//--------------------------------------------------------------------------------//
+// Umsetzung Stand 17.07: 
+// Transaction: (Deploy), Partner, Counter Limit, Balance Limit, increase, Confirmation Owner, Confirmation Partner
+// Call: stamp Owner, stamp Partner, Counter Limit, Balance Limit, Balance , Count, Confirmation Owner, Confirmation Partner, Check Balance Limit 
+//--------------------------------------------------------------------------------//
+//TODO: Check Confirmation and Send 
 //--------------------------------------------------------------------------------//
 const web3Abi = require('web3-eth-abi')
 const Tx = require('ethereumjs-tx');
@@ -12,8 +22,8 @@ var accounts1 = "0x6976D2536659d8e922AC7c462a2A5D425baD9c94"
 //1.3 Betrüger: 
 var accounts2 = "0xE4a19Ec71e3a83065254B61b6b079dc7A2d9c9C8"
 //2 Smart Contracts: 
-var CaD = "0x588faa45a03de10ac104c2fa06a9e8f89099ccc7"
 //2.1 CountAndDeposit:
+var CaD = "0x588faa45a03de10ac104c2fa06a9e8f89099ccc7"
 //--------------------------------------------------------------------------------//
 //3 Transactions:
 //3.0 Deploy Smart Contract
@@ -33,7 +43,15 @@ web3.eth.sendTransaction({
          name: 'setCounterLimit', type: 'function', inputs: [{type: 'uint256', name: 'input'}]},
          ['300'])
     })
-//3.3 Increase Balance of Smart Contract and Working Hours
+//3.3 Define BalanceLimit 
+web3.eth.sendTransaction({
+    from: accounts0,
+    to: CaD,
+     data:web3.eth.abi.encodeFunctionCall({
+         name: 'setBalanceLimit', type: 'function', inputs: [{type: 'uint256', name: 'input'}]},
+         ['2'])
+    })
+//3.4 Increase Balance of Smart Contract and Working Hours
 web3.eth.sendTransaction({
     from: accounts0,
     to: CaD, 
@@ -41,20 +59,21 @@ web3.eth.sendTransaction({
     data:web3.eth.abi.encodeFunctionCall({name: 'increase', type: 'function', inputs: [{type: 'uint256', name: 'input'}]},
     ['200'])
     })
-//3.4 Confirmatation of the Owner
+//3.5 Confirmatation of the Owner
 web3.eth.sendTransaction({
     from: accounts0,
     to: CaD, 
     data:web3.eth.abi.encodeFunctionCall({name: 'setConfirmationOwner', type: 'function', inputs: [{type: 'uint256', name: 'input'}]},
     ['1'])
     })
-//3.5 Confirmatation of the Partner
+//3.6 Confirmatation of the Partner
 web3.eth.sendTransaction({
     from: accounts1,
     to: CaD,
      data:web3.eth.abi.encodeFunctionCall({name: 'setConfirmationOwner', type: 'function', inputs: [{type: 'uint256', name: 'input'}]},
      ['1'])
     })
+//3.7 Check Confirmation and Send 
 //--------------------------------------------------------------------------------//
 //4 Calls: 
 //4.1 Get value of the Counterlimit
@@ -63,29 +82,48 @@ web3.eth.call({
     data:web3Abi.encodeFunctionSignature('getCounterLimit()')},
     (err, counterLimit) => {var CL = web3.utils.toDecimal(counterLimit); console.log(CL)
     });
-//4.2 Get the current Balance of the Smart Contract
+//4.2 Get value of the BalanceLimit 
+web3.eth.call({
+    to: CaD, 
+    data:web3Abi.encodeFunctionSignature('getBalanceLimit()')},
+    (err, balanceLimit) => {var BL = web3.utils.toDecimal(balanceLimit); console.log(BL)
+    });
+//4.3 Get the current Balance of the Smart Contract
 web3.eth.getBalance(CaD).then(function(getBalance){console.log(getBalance);}).catch(function(e){console.log(e);});
-//4.3 Get the current WorkingHours of the Smart Contract
+//4.4 Get the current WorkingHours of the Smart Contract
 web3.eth.call({
     to:CaD, 
     data:web3Abi.encodeFunctionSignature('getCount()')},
     (err, machineCounter) => {var MC = web3.utils.toDecimal(machineCounter); console.log(MC)
     });
-//4.4 Show if CounterLimit is already reached
+//4.5 Show if CounterLimit is already reached
 web3.eth.call({
     to:CaD, 
     data:web3Abi.encodeFunctionSignature('checkCounterLimit()')})
-//4.5 Show if Owner confirmed
+//4.6 Show if Owner confirmed
 web3.eth.call({
     to:CaD, 
     data:web3Abi.encodeFunctionSignature('getConfirmationOwner()')},
     (err, ConfirmationOwner) => {var CO = web3.utils.toDecimal(ConfirmationOwner); console.log(CO)});
-//4.6 Show if Partner confirmed
+//4.7 Show if Partner confirmed
 web3.eth.call({
     to:CaD, 
     data:web3Abi.encodeFunctionSignature('getConfirmationPartner()')},
     (err, ConfirmationPartner) => {var CP = web3.utils.toDecimal(ConfirmationPartner); console.log(CP)
     });
+//4.8 get Stamp of Contract Owner 
+web3.eth.call({
+    to:CaD, 
+    data:web3Abi.encodeFunctionSignature('getStampOwner()')},
+    (err, stampOwner) => {var SO = web3.utils.toDecimal(stampOwner); console.log(SO)
+    });
+//4.9 get Stamp of Contract Partner 
+web3.eth.call({
+    to:CaD, 
+    data:web3Abi.encodeFunctionSignature('getStampPartner()')},
+    (err, stampPartner) => {var SP = web3.utils.toDecimal(stampPartner); console.log(SP)
+    });
+
 
 
 
