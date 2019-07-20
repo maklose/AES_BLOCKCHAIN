a7042f45c7824dc6c204cb1aaf7dea2a254c2c90
hthuelig <51367725+hthuelig@users.noreply.github.com>
2019-07-20 14:55:54 +0200

    hgj

--- contracts/ContractA.sol
+++ contracts/ContractA.sol
@@ -26,41 +26,26 @@ contract CountAndDeposit {
          contractOwner = msg.sender;
      }
      
-// HIER DEFINIEREN WIR UNSERE REQUIRE MODIFIER
-     // Nur der contractOwner darf die Funktion ausführen
-     modifier _Owner {
-         require (msg.sender == contractOwner);
-         _;
-     }
-     
-     // Nur der contractPartner darf die Funktion ausführen
-     modifier _Partner {
-         require (msg.sender == contractPartner);
-         _;
-     }
-     
-     // Nur der contractOwner und der contractPartner dürfen die Funktion ausführen
-     modifier _OwnerAndPartner {
-         require (msg.sender == contractOwner || msg.sender == contractPartner);
-         _;
-     }
 
 // HIER DEFINIEREN WIR UNSERE INPUT VARIABLEN     
      // hier wird der contractPartner definiert
      // kann nur vom contractOwner definiert werden
-     function setContractPartner(address payable input) public _Owner{
+     function setContractPartner(address payable input) public {
+         require (msg.sender == contractOwner);
          contractPartner = input;
      }
      
      // legt das Limit für Maschinenstunden fest
      // kann nur vom contractOwner definiert werden
-     function setCounterLimit(uint input) public _Owner{
+     function setCounterLimit(uint input) public {
+         require (msg.sender == contractOwner);
          counterLimit = input;
      }
      
      // legt das Limit für Maschinenstunden fest
      // kann nur vom contractOwner definiert werden
-     function setBalanceLimit(uint input) public _Owner{
+     function setBalanceLimit(uint input) public {
+         require (msg.sender == contractOwner);
          balanceLimit = input;
      } 
      
@@ -68,7 +53,8 @@ contract CountAndDeposit {
 // INPUT
     // erhöht Counter für Maschinenstunden des owners
     // (also der Maschine, die den Contract angelegt hat)
-    function increase(uint256 input) public payable _Owner{
+    function increase(uint256 input) public payable {
+        require (msg.sender == contractOwner);
         machineCounter[contractOwner] += input;
         machineBalance[contractOwner] +=msg.value;
     }
@@ -89,11 +75,13 @@ contract CountAndDeposit {
         stampPartner = now;
     }
     
-     function getStampOwner() public view _Owner returns (uint)  {
+     function getStampOwner() public view returns (uint)  {
+         require (msg.sender == contractOwner);
          return stampOwner;
      }
      
-     function getStampPartner() public view _Owner returns (uint) {
+     function getStampPartner() public view returns (uint) {
+         require (msg.sender == contractOwner);
          return stampPartner;
      }
 
@@ -102,61 +90,71 @@ contract CountAndDeposit {
 // OUTPUT
      // gibt counterLimit zurück
      // kann von allen genutzt werden
-     function getCounterLimit() public view _OwnerAndPartner returns (uint) {
+     function getCounterLimit() public view returns (uint) {
+             require (msg.sender == contractOwner || msg.sender == contractPartner);
              return counterLimit;
      }
     
      // gibt balanceLimit zurück
      // kann von allen genutzt werden
-     function getBalanceLimit() public view _OwnerAndPartner returns (uint) {
+     function getBalanceLimit() public view returns (uint) {
+             require (msg.sender == contractOwner || msg.sender == contractPartner);
              return balanceLimit;
      }
      
     // LEDIGLICH FÜR TESTZWECKE [bei finalem Deployment nicht mehr im Code]
         // gibt Adresse des contractPartner zurück
         // kann nur vom contractOwner genutzt werden
-        function getContractPartner() public view _Owner returns (address) {
+        function getContractPartner() public view returns (address) {
+            require (msg.sender == contractOwner);
             return contractPartner;
         }
     
         // gibt Adresse des contractPartner zurück
         // kann nur vom contractOwner genutzt werden
-        function getContractOwner() public view _OwnerAndPartner returns (address) {
+        function getContractOwner() public view returns (address) {
+            require (msg.sender == contractOwner || msg.sender == contractPartner);
             return contractOwner;
         }
     
         // gibt Adresse des Contracts zurück
         // kann nur vom contractOwner genutzt werden
-        function getContractAddress() public view _OwnerAndPartner returns (address) {
+        function getContractAddress() public view returns (address) {
+            require (msg.sender == contractOwner || msg.sender == contractPartner);
             return address(this);
         }
     
     // überprüft und gibt die Balance des owners 
     // (also die Einzahlungen der Maschine) zurück
-    function getBalance() public view _OwnerAndPartner returns (uint) {
+    function getBalance() public view returns (uint) {
+            require (msg.sender == contractOwner || msg.sender == contractPartner);
             return machineBalance[contractOwner];
     }
     
     // überprüft und gibt den Counter des owners
     // (also die Maschinenstunden der Maschine) zurück
-    function getCount() public view _OwnerAndPartner returns (uint) {
+    function getCount() public view returns (uint) {
+           require (msg.sender == contractOwner || msg.sender == contractPartner);
            return machineCounter[contractOwner];
     }
 
     // gibt Signal zurück, ob Maschinenbestätigung eingegangen ist, oder nicht
-    function getConfirmationOwner() public view _OwnerAndPartner returns (uint) {
+    function getConfirmationOwner() public view returns (uint) {
+           require (msg.sender == contractOwner || msg.sender == contractPartner);
            return ConfirmationOwner;
     }
     
     // gibt Signal zurück, ob Bestätigung des Dienstleisters eingegangen ist, oder nicht
-    function getConfirmationPartner() public view _OwnerAndPartner returns (uint) {
+    function getConfirmationPartner() public view returns (uint) {
+           require (msg.sender == contractOwner || msg.sender == contractPartner);
            return ConfirmationPartner;
     }
 
     // überprüft, ob von beiden Parteien (Maschine und Dienstleister) die Bestätigungen eingegangen
     // sind
     // gibt Ja/Nein Wert aus
-    function checkConfirmationAndSendPayment() public _OwnerAndPartner {
+    function checkConfirmationAndSendPayment() public {
+         require (msg.sender == contractOwner || msg.sender == contractPartner);
          if (ConfirmationOwner == 1 && ConfirmationPartner == 1) {
          contractPartner.transfer(address(this).balance);
         }
@@ -168,7 +166,8 @@ contract CountAndDeposit {
     
     // Checkt das CounterLimit(balance) und sendet automatisch eine Zahlung an den ContractPartner 
     // in Höhe des Smart Contrat Limits
-    function checkCounterLimit() public view _OwnerAndPartner returns (uint) {
+    function checkCounterLimit() public view returns (uint) {
+        require (msg.sender == contractOwner || msg.sender == contractPartner);
         if (address(this).balance >= balanceLimit) return (1);
         else return (0);
     }
