7038d7eead4b37e41b5ad3ee3eef9d194d6395e2
hthuelig <51367725+hthuelig@users.noreply.github.com>
2019-07-29 07:57:09 +0200

    as

--- contracts/PayPerUse_Test.sol
+++ contracts/PayPerUse_Test.sol
@@ -1,6 +1,54 @@
 pragma solidity >=0.4.25 <0.7.0;
 
-contract PayPerUse {
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+    
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+}
+
+
+    contract PayPerUse {
+    
+    using SafeMath for uint256;
  
     address payable contractOwner;
     address payable contractPartner;
@@ -10,6 +58,7 @@ contract PayPerUse {
     uint256 transferAmount;
     uint price;
     uint balanceLimit;
+    uint restAmount;
     
     constructor() public {
         contractOwner = msg.sender;
@@ -26,12 +75,28 @@ contract PayPerUse {
         price = input;
     }
     
+    function getPrice() public view returns(uint) {
+        return price;
+    }
+    
     function increase(uint256 input) public payable {
-        require (msg.sender == contractOwner);
+        require ((msg.sender == contractOwner));
         machineCounter += input;
-        transferAmount == machineCounter * price;
-        
-      //  contractPartner.transfer(balanceLimit * 10**18);
+        transferAmount = (machineCounter * price);
+        contractPartner.transfer(transferAmount * 10**18);
+    }
+    
+    function getTransferAmount() public view returns (uint) {
+        return transferAmount;
+    }
+    
+    function payService() public payable{
+        contractPartner.transfer(transferAmount * 10**18);
+    }
+    
+    function refund() public payable {
+        restAmount = (address(this).balance);
+        contractOwner.transfer(restAmount);
     }
     
      // überprüft und gibt die Balance des owners 
